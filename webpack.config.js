a7524511429721ee60060158d801baff06b59e94
sshrik <chj878194@naver.com>
2021-07-12 16:46:23 +0900

    feature - babel 통합 완료

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,19 @@ const config = {
     filename: '[name].js'
   },
   module: {
-
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-throw-expressions"]
+          }
+        }
+      }
+    ]
   },
   plugins: [],
   optimization: {},
