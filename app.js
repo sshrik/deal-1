60d332556b41da4fcc64ca470faa2562f103d216
sshrik <chj878194@naver.com>
2021-07-12 22:50:11 +0900

    feature - app.js 에서 이제 정확한 경로에 있는 파일을 불러옵니다. * app.js : public/dist를 view로 설정. * ejs 를 설치해서 html 을 parsing 하도록 했습니다.

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const { bundle } = require("./webpack.config");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("views", path.join(__dirname, "public/dist"));
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
 
 if(process.env.NODE_ENV === "dev"){
   const liveServer = livereload.createServer({
@@ -29,7 +31,7 @@ if(process.env.NODE_ENV === "dev"){
         liveServer.refresh("/");
       }, 100);
     });
-  })
+  });
 }
 
 app.use(logger('dev'));
@@ -38,11 +40,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -57,7 +57,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error.html");
 });
 
 module.exports = app;
