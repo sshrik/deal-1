ac49e95445ce40cc12a1b4626ea1816bf6786b5b
LeeJaeyun <ag502@naver.com>
2021-07-13 16:41:17 +0900

    fix : dir 구조 변경

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const livereload = require('livereload');
 const { bundle } = require('./webpack.config');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -42,23 +40,10 @@ app.use(express.static(path.join(__dirname, 'public/dist')));
 app.use(express.static(path.join(__dirname, 'public/resource')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log(err);
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error.html');
 });
 
 module.exports = app;
