8e3f12ac075df39abfb316ac991d4a22feac6d41
sshrik <chj878194@naver.com>
2021-07-13 09:10:08 +0900

    feature - localhost 환경 ( dev ) 에서만 auto reload script를 불러오게 변경.

--- asset/globalInit.js
+++ asset/globalInit.js
@@ -1,8 +1,11 @@
 export function addLiveReloadScript() {
   // Tag 에 liveReload Script를 붙여주어 자동으로 reload하게 해 줍니다.
-  const liveReloadScript = document.createElement('script');
-  liveReloadScript.async = true;
-  liveReloadScript.defer = true;
-  liveReloadScript.src = 'http://localhost:35729/livereload.js?snipver=1';
-  document.head.appendChild(liveReloadScript);
+  if(document.location.host.split(":")[0] == "localhost") { 
+    // localhost에서 수행한 경우만 실행합니다.
+    const liveReloadScript = document.createElement('script');
+    liveReloadScript.async = true;
+    liveReloadScript.defer = true;
+    liveReloadScript.src = 'http://localhost:35729/livereload.js?snipver=1';
+    document.head.appendChild(liveReloadScript);
+  }
 };
