7dee7c7b910eb86559c5a50d0c72c6c8574f87f0
LeeJaeyun <ag502@naver.com>
2021-07-13 11:19:17 +0900

    feature : file-loader 설치

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const livereload = require("livereload");
-const { bundle } = require("./webpack.config");
+const livereload = require('livereload');
+const { bundle } = require('./webpack.config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,22 +13,22 @@ var loginRouter = require('./routes/login');
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "public/dist"));
+app.set('views', path.join(__dirname, 'public/dist'));
 app.engine('html', require('ejs').renderFile);
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-if(process.env.NODE_ENV === "dev"){
+if (process.env.NODE_ENV === 'dev') {
   const liveServer = livereload.createServer({
-    exts: ["js", "html", "css"],
-  });  
+    exts: ['js', 'html', 'css'],
+  });
 
   app.locals.env = process.env;
 
   bundle().then(() => {
-    liveServer.watch([path.resolve("public/dist"), path.resolve("views")]);
-    liveServer.server.once("connection", () => {
+    liveServer.watch([path.resolve('public/dist'), path.resolve('views')]);
+    liveServer.server.once('connection', () => {
       setTimeout(() => {
-        liveServer.refresh("/");
+        liveServer.refresh('/');
       }, 100);
     });
   });
@@ -38,11 +38,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public/dist")));
+app.use(express.static(path.join(__dirname, 'public/dist')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/login", loginRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -53,11 +53,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err);
   // render the error page
   res.status(err.status || 500);
-  res.render("error.html");
+  res.render('error.html');
 });
 
 module.exports = app;
