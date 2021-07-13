44baec882543240f20294c20fc4ac6f462d0c32d
LeeJaeyun <ag502@naver.com>
2021-07-13 18:49:41 +0900

    feature : class 컨트롤 함수 추가
    - asset/util/domcontroll에 element 상속함수 추가

--- asset/util/domControll.js
+++ asset/util/domControll.js
@@ -1,3 +1,18 @@
+Element.prototype.addClass = function (className) {
+  this.classList.add(className);
+  return this;
+};
+
+Element.prototype.removeClass = function (className) {
+  this.classList.remove(className);
+  return this;
+};
+
+Element.prototype.toggleClass = function (className) {
+  this.classList.toggle(className);
+  return this;
+};
+
 export default {
   find: (selector) => document.querySelector(selector),
   create: (tagName) => document.createElement(tagName),
