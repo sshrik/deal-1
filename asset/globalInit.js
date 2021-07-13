83faadb218fcf04b146a615103b227458c500e06
sshrik <chj878194@naver.com>
2021-07-13 09:13:34 +0900

    feature - 코드를 추가하지 않고 그냥 import 만 추가하면 되게 변경.

--- asset/globalInit.js
+++ asset/globalInit.js
@@ -9,3 +9,8 @@ export function addLiveReloadScript() {
     document.head.appendChild(liveReloadScript);
   }
 };
+
+// Add Live Reload Script. 
+// 이 코드를 수행하면 서버에 있는 파일이 변경될 때 마다 0.1초마다 자동으로 reload 해서 실행해줍니다.
+// 이는 npm run dev 로 수행한 경우에만 해당합니다.
+addLiveReloadScript();
--- asset/view/login.js
+++ asset/view/login.js
@@ -1,9 +1,4 @@
 import "../css/test.css";
-import { addLiveReloadScript } from "../globalInit.js";
-
-// Add Live Reload Script. 
-// 이 코드를 수행하면 서버에 있는 파일이 변경될 때 마다 0.1초마다 자동으로 reload 해서 실행해줍니다.
-// 이는 npm run dev 로 수행한 경우에만 해당합니다.
-addLiveReloadScript();
+import "../globalInit.js";
 
 console.log("Hello world!");
