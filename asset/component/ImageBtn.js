de9a4f326afba59cc5402c0b32a48b2f8442345f
LeeJaeyun <ag502@naver.com>
2021-07-15 02:03:15 +0900

    feature : file reader 구현

---
+++ asset/component/ImageBtn.js
@@ -0,0 +1,24 @@
+import $ from '../util/domControll';
+import icons from './icons';
+
+export default (type, numOfFiles, src = null, deleteAction = null) => {
+  const $imgContainer = $.create('div').addClass('img').addClass('img-medium');
+
+  if (type === 'add') {
+    const $pictureCount = $.create('div');
+    $pictureCount.appendChild(icons.picture());
+    $pictureCount.appendChild($.create('span').setText(`10/${numOfFiles}`));
+    $imgContainer.appendChild($pictureCount);
+  } else if (type === 'delete') {
+    const $img = $.create('img');
+    $img.src = src;
+
+    const $eraseBtn = $.create('div').addClass('erase-btn');
+    $eraseBtn.appendChild(icons.erase());
+
+    $imgContainer.appendChild($img);
+    $imgContainer.appendChild($eraseBtn);
+  }
+
+  return $imgContainer;
+};
