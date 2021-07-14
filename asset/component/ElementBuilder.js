664303d61b669c802eef7b168c9cee685cc9310e
sshrik <chj878194@naver.com>
2021-07-14 10:26:57 +0900

    feature - 반복문 대신 바로 내부 요소 삭제.

--- asset/component/ElementBuilder.js
+++ asset/component/ElementBuilder.js
@@ -52,9 +52,7 @@ export default class ElementBuilder {
 
     // 만약 App 하나만 달고 싶다면 clearAll 을 설정해줍니다. 다만 맨 윗 node에서 호출할 때만 사용됩니다. ( Page 단위로만.. )
     if (clearAll) {
-      while (DOMDestParent.hasChildNodes()) {
-        DOMDestParent.removeChild(DOMDestParent.firstChild);
-      }
+      DOMDestParent.innerHTML = '';
     }
     // 루트 노드의 맨 마지막에 자신을 추가해줍니다.
     DOMDestParent.appendChild(this.contents);
