f851b270217dfb245b920a7a25609bdc7615a997
sshrik <chj878194@naver.com>
2021-07-15 23:22:03 +0900

    fix - Category 중복되는것 상수로 뺐음.

--- asset/component/CategorySelector.js
+++ asset/component/CategorySelector.js
@@ -1,6 +1,7 @@
 import $ from '../util/domControll';
 import ElementBuilder from './ElementBuilder';
 import CategorySelectContainer from './CategorySelectorContainer';
+import categories from './CategoryList';
 
 export default class CategorySelector extends ElementBuilder {
   constructElement() {
@@ -12,23 +13,6 @@ export default class CategorySelector extends ElementBuilder {
     const $selectCategory =
       $.create('p').setText('(필수)카테고리를 선택해주세요.');
     $element.appendChild($selectCategory);
-
-    const categories = [
-      '디지털기기',
-      '생활가전',
-      '가구/인테리어',
-      '게임/취미',
-      '생활/가공식품',
-      '스포츠/레저',
-      '여성패션/잡화',
-      '남성패션/잡화',
-      '유아동',
-      '뷰티/미용',
-      '반려동물',
-      '도서/티켓/음반',
-      '식물',
-      '기타 중고물품',
-    ];
 
     new CategorySelectContainer({
       parent: this,
--- asset/component/CategorySelectorContainer.js
+++ asset/component/CategorySelectorContainer.js
@@ -1,26 +1,11 @@
 import $ from '../util/domControll';
 import ElementBuilder from './ElementBuilder';
 import CategoryButton from './CategoryButton';
+import categories from './CategoryList';
 
 export default class CategorySelectContainer extends ElementBuilder {
   constructElement() {
     const $element = $.create('div').addClass('category-selector__container');
-    const categories = [
-      '디지털기기',
-      '생활가전',
-      '가구/인테리어',
-      '게임/취미',
-      '생활/가공식품',
-      '스포츠/레저',
-      '여성패션/잡화',
-      '남성패션/잡화',
-      '유아동',
-      '뷰티/미용',
-      '반려동물',
-      '도서/티켓/음반',
-      '식물',
-      '기타 중고물품',
-    ];
     categories.forEach((element, index) => {
       new CategoryButton({
         parent: this,
