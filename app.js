c686a38b00eac87819d3f9e00fe6ea764e67a3d2
LeeJaeyun <ag502@naver.com>
2021-07-17 15:32:00 +0900

    feature - db 연결

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const pool = require('./db');
 const livereload = require('livereload');
 const { bundle } = require('./webpack.config');
 
