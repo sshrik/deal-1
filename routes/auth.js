ee73b98266935c55604912ed260bdff9ad56e63c
LeeJaeyun <ag502@naver.com>
2021-07-17 17:18:50 +0900

    fix - login api 응답 형태 수정

--- routes/auth.js
+++ routes/auth.js
@@ -6,17 +6,16 @@ const router = express.Router();
 router.post('/login', (req, res) => {
   const { userName, password } = req.body;
   pool.execute(getUser, [userName], (err, results, field) => {
-    console.log(results);
     if (err) {
-      return res.status(500).send('회원 정보를 찾을 수 없습니다.');
+      return res.status(500).json({ error: '회원 정보를 찾을 수 없습니다.' });
     }
     if (results.length === 0) {
-      return res.status(401).send('아이디를 확인해 주세요.');
+      return res.status(401).json({ error: '아이디를 확인해 주세요.' });
     }
     if (results[0].passWord !== password) {
-      return res.status(401).send('비밀번호를 확인해 주세요.');
+      return res.status(401).json('비밀번호를 확인해 주세요.');
     }
-    res.status(200).send('로그인 성공');
+    res.status(200).json({ userName });
   });
 });
 
