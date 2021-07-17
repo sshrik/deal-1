4f65a05d37eb78842ce4142f73ec5940943708c0
LeeJaeyun <ag502@naver.com>
2021-07-17 14:45:06 +0900

    feature - password 인풋 추가

--- asset/pages/Login/LoginContainer.js
+++ asset/pages/Login/LoginContainer.js
@@ -19,6 +19,13 @@ export default class LoginContainer extends ElementBuilder {
       id: 'id',
       placeHolder: '아이디를 입력하세요',
     });
+    new Input({
+      parent: this,
+      type: 'password',
+      size: 'large',
+      id: 'password',
+      placeHolder: '비밀번호를 입력하세요',
+    });
     new Button({
       parent: this,
       text: '로그인',
