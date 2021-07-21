c1ac38f5e0b2d076c7a1bf4a5b6d7daf7f7e0a0a
sshrik <chj878194@naver.com>
2021-07-22 01:09:04 +0900

    fix - console 지우.

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -19,7 +19,6 @@ router.get('/products_user', async (req, res) => {
   try {
     const bmCookie = req.cookies.bmCookie;
     const id = req.session[bmCookie];
-    console.log(id);
     // access middle ware 설정 후 변경 예정
     const [results, _] = await pool.execute(getAllProductsAuth, [id]);
     util.sendJson(res, { data: results });
