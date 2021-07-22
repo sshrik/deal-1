717e400eeb1e91b9f4e73fd9af5fad75cc20bb07
sshrik <chj878194@naver.com>
2021-07-22 23:25:33 +0900

    feature - add SQL

--- backend/model/query/chat.js
+++ backend/model/query/chat.js
@@ -1,6 +1,5 @@
-const enterChattingRoom = `INSERT INTO chatrooms (productId, userName, lastview)
-VALUES(?, (SELECT id FROM users WHERE userName= ? ), ?),
-(?, (SELECT id FROM users WHERE userName= ? ), ?);`;
+const enterChattingRoom = `INSERT INTO chatrooms (productId, user1, user2, lastview)
+VALUES(?, (SELECT id FROM users WHERE userName= ? ), (SELECT id FROM users WHERE userName= ? ), ?);`;
 
 const getChatLog = `SELECT chatMsg, sendTime, type from chats where productId = ? and (
   ( sendName =(SELECT id FROM users WHERE userName= ? )and recvName = (SELECT id FROM users WHERE userName= ? ) ) or
@@ -10,5 +9,10 @@ const getChatLog = `SELECT chatMsg, sendTime, type from chats where productId = 
 const sendChat = `
 INSERT INTO chats ( sendName, recvName, productId, chatMsg, sendTime, type)
 VALUE(  (SELECT id FROM users WHERE userName= ? ), (SELECT id FROM users WHERE userName= ? ), ?, ?, ?, 'chat');`;
+
+const getChattingRoom = `SELECT COUNT(*) from chatrooms 
+WHERE productId = ? AND 
+user1 = (SELECT id FROM users WHERE userName = '?') AND 
+user2 = (SELECT id FROM users WHERE userName = ?);`;
 
 module.exports = { enterChattingRoom, getChatLog, sendChat };
